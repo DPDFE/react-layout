fe2e16a211e66805d39ffe74a561576d08708b86
pearone <1181019452@qq.com>
2022-01-27 12:59:46 +0800

    chore:测试lerna版本同时更新效果

--- packages/event-utils/lib/event-utils.js
+++ packages/event-utils/lib/event-utils.js
@@ -1,20 +1,9 @@
 'use strict';
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 /** 添加event */
 export function addEvent(el, event, handler, inputOptions) {
     if (!el)
         return;
-    var options = __assign({ capture: true }, inputOptions);
+    const options = Object.assign({ capture: true }, inputOptions);
     if (el.addEventListener) {
         el.addEventListener(event, handler, options);
     }
@@ -29,7 +18,7 @@ export function addEvent(el, event, handler, inputOptions) {
 export function removeEvent(el, event, handler, inputOptions) {
     if (!el)
         return;
-    var options = __assign({ capture: true }, inputOptions);
+    const options = Object.assign({ capture: true }, inputOptions);
     if (el.removeEventListener) {
         el.removeEventListener(event, handler, options);
     }
@@ -40,4 +29,3 @@ export function removeEvent(el, event, handler, inputOptions) {
         el['on' + event] = null;
     }
 }
-//# sourceMappingURL=event-utils.js.map
