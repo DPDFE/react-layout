62d77906e57d83d46f62587220d9e5a0101a51b2
pearone <1181019452@qq.com>
2022-03-10 10:08:35 +0800

    update

--- example/src/demo/ruler/index.tsx
+++ example/src/demo/ruler/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { ReactDragLayout, LayoutType } from 'react-drag-layout';
+import {
+    ReactDragLayout,
+    LayoutType,
+    ReactLayoutContext
+} from 'react-drag-layout';
 import 'react-drag-layout/dist/index.css';
 
 const RulerLayout = () => {
@@ -7,18 +11,24 @@ const RulerLayout = () => {
         <div
             style={{ display: 'flex', flexDirection: 'column', height: '100%' }}
         >
-            <ReactDragLayout need_ruler></ReactDragLayout>
+            <ReactLayoutContext>
+                <ReactDragLayout need_ruler></ReactDragLayout>
+            </ReactLayoutContext>
             <div style={{ display: 'flex', flex: 1, height: '50%' }}>
-                <ReactDragLayout
-                    need_ruler
-                    layout_type={LayoutType.DRAG}
-                ></ReactDragLayout>
-                <ReactDragLayout
-                    need_ruler
-                    width={1280}
-                    height={1280}
-                    layout_type={LayoutType.DRAG}
-                ></ReactDragLayout>
+                <ReactLayoutContext>
+                    <ReactDragLayout
+                        need_ruler
+                        layout_type={LayoutType.DRAG}
+                    ></ReactDragLayout>
+                </ReactLayoutContext>
+                <ReactLayoutContext>
+                    <ReactDragLayout
+                        need_ruler
+                        width={1280}
+                        height={1280}
+                        layout_type={LayoutType.DRAG}
+                    ></ReactDragLayout>
+                </ReactLayoutContext>
             </div>
         </div>
     );
