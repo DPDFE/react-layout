5d46c1cfb4258a445cad9fbcb5db4d3c1d306819
pearone <1181019452@qq.com>
2022-03-14 18:00:42 +0800

    update

--- packages/event-utils/lib/event-utils.js
+++ packages/event-utils/lib/event-utils.js
@@ -2,35 +2,39 @@
 'use strict';
 /** 添加event */
 export function addEvent(el, event, handler, inputOptions) {
-    if (!el) return;
+    if (!el)
+        return;
     const options = Object.assign({ capture: false }, inputOptions);
     if (el.addEventListener) {
         el.addEventListener(event, handler, options);
-    } else if (el.attachEvent) {
+    }
+    else if (el.attachEvent) {
         el.attachEvent('on' + event, handler);
-    } else {
+    }
+    else {
         el['on' + event] = handler;
     }
 }
 /** 删除event */
 export function removeEvent(el, event, handler, inputOptions) {
-    if (!el) return;
+    if (!el)
+        return;
     const options = Object.assign({ capture: false }, inputOptions);
     if (el.removeEventListener) {
         el.removeEventListener(event, handler, options);
-    } else if (el.detachEvent) {
+    }
+    else if (el.detachEvent) {
         el.detachEvent('on' + event, handler);
-    } else {
+    }
+    else {
         el['on' + event] = null;
     }
 }
 export function isFunction(func) {
-    return (
-        typeof func === 'function' ||
-        Object.prototype.toString.call(func) === '[object Function]'
-    );
+    return (typeof func === 'function' ||
+        Object.prototype.toString.call(func) === '[object Function]');
 }
-let matches_selector_func = '';
+let matches_selector_func = undefined;
 export function matchesSelector(el, selector) {
     const matches_methods = [
         'matches',
@@ -40,23 +44,27 @@ export function matchesSelector(el, selector) {
         'oMatchesSelector'
     ];
     if (!matches_selector_func) {
-        matches_methods.map((method) => {
+        matches_selector_func = matches_methods
+            .map((method) => {
             // @ts-ignore
-            matches_selector_func = isFunction(el[method]) ? el[method] : '';
-        });
+            return isFunction(el[method]) ? method : '';
+        })
+            .find((state) => state != '');
     }
     // @ts-ignore
-    if (!isFunction(el[matches_selector_func])) return false;
+    if (!isFunction(el[matches_selector_func]))
+        return false;
     // @ts-ignore
-    console.log(el[matches_selector_func], selector);
     return el[matches_selector_func](selector);
 }
 /** 找到从当前元素一直找到baseNode是否有selector */
 export function matchesSelectorAndParentsTo(el, selector, baseNode) {
     let node = el;
     do {
-        if (matchesSelector(node, selector)) return true;
-        if (node === baseNode) return false;
+        if (matchesSelector(node, selector))
+            return true;
+        if (node === baseNode)
+            return false;
         node = node.parentNode;
     } while (node);
     return false;
