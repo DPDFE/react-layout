b804a22b5fe284d59e015635761c3d3b7057f516
pearone <1181019452@qq.com>
2022-03-14 14:57:12 +0800

    update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,10 +3,6 @@ export function fomatFloatNumber(num: number, precision: number) {
     return Math.round(num * Math.pow(10, precision)) / Math.pow(10, precision);
 }
 
-export function copyObjectArray<T>(arr: T[]): T[] {
-    return [].concat(JSON.parse(JSON.stringify(arr)));
-}
-
 // TODO：函数拷贝不上，直接挂载到Object => Object.copy
 export function copyObject<T>(obj: T): T {
     return JSON.parse(JSON.stringify(obj));
