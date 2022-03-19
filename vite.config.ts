4dee4e161cf441a3574ea15db2d0cc80a211ada9
pearone <1181019452@qq.com>
2022-03-19 16:41:32 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true,
+            },
+        },
+    },
     plugins: [react()],
 });
