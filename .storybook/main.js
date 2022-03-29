0999916f219054b680a460abd8560132e889e18a
pearone <1181019452@qq.com>
2022-03-29 19:17:56 +0800

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,8 +8,7 @@ module.exports = {
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-        'storybook-addon-code-preview'
+        '@storybook/addon-interactions'
     ],
     framework: '@storybook/react',
     core: {
