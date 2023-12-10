284a10c866b08f16c35c76193d599c0b366b4ef7
pearone <1181019452@qq.com>
2023-12-10 08:57:10 +0800

    test: update

--- packages/event/__tests__/dag.test.ts
+++ packages/event/__tests__/dag.test.ts
@@ -4,12 +4,12 @@ test('dag', () => {
     const dag = new DAG({
         nodes: [1, 2, 3, 4, 5],
         links: [
-            { parent_id: 1, child_id: 2, name: 111 },
-            { parent_id: 2, child_id: 3, name: 222 },
-            { parent_id: 2, child_id: 4, name: 222 },
-            { parent_id: 3, child_id: 1, name: 333 },
-            { parent_id: 4, child_id: 5, name: 444 }
-            // { parent_id: 4, child_id: 6, name: 444 }
+            { parent_id: 1, child_id: 2, type: 'link' },
+            { parent_id: 2, child_id: 3, type: 'drill' },
+            { parent_id: 2, child_id: 4, type: 'filter' },
+            { parent_id: 3, child_id: 1, type: 'filter' },
+            { parent_id: 4, child_id: 5, type: 'link' },
+            { parent_id: 4, child_id: 6, type: 'filter' }
         ],
         link_key: {
             start: 'parent_id',
