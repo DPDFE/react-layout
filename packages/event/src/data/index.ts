8b7fc34f64db0a56219fcb70bb110e5f0f2269f5
pearone <1181019452@qq.com>
2024-03-15 15:57:47 +0800

    update

---
+++ packages/event/src/data/index.ts
@@ -0,0 +1,9 @@
+export {
+    isNumber,
+    formatFloatNumber,
+    toFixedNumber,
+    formatNumberToPercentString,
+    getThousandthSeparatedNumber,
+    formatNumberUnit,
+    readableNumbers
+} from './format';
